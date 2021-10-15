95c61c83db6dfeb7baba4ff41b51127513dd136f
kevans107 <kellyevans107.gmail.com>
2021-10-14 22:49:25 -0400

    about me page functional

---
+++ script.js
@@ -0,0 +1,7 @@
+// typing animation script
+var typed = new Typed("#typing",{
+    strings: ["Mom", "Army Veteran", "Full Stack Developer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+})
